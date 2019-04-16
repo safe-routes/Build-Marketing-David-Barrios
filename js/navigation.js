94c342107db151bbe5f584e45b0002cc9ef22b91
David Barrios <barriosadavid@gmail.com>
2019-04-16 15:46:14 -0700

    fixed a couple of bugs added some content

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,6 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const btnSpan = document.querySelector('.hamburger span');
 const menu = document.querySelector('.menu');
-const links = document.querySelector('.menu ul');
 
 hamburgerBtn.addEventListener('click', () => {
     btnSpan.classList.toggle('hamburgerAni');
